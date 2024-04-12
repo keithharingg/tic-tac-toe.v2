3716644f2951c2d5a21a6817bf61abbbebfab91c
Taras Nesterenko <tnestere@gmail.com>
2024-04-12 13:06:49 +0200

    added fonts and logo

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
+import logoSrc from './logo.svg';
 
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex h-24 items-center px-8 bg-white shadow-lg">
+      <Image src={logoSrc} alt="Logo" />
+      <div className="w-px h-8 bg-slate-300 mx-6" />
+      <button className="w-44 bg-teal-600 text-white rounded-lg px-5 py-2">Play</button>
+    </header>
+  );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,15 @@
 import '../styles/global.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
